1c13d52cad1ce1a11f6fd39fdd33c6f28b4703dd
allansto <allan.sto@gmail.com>
2018-10-19 16:21:35 -0400

    fixed creating config sample

--- configulator.py
+++ configulator.py
@@ -13,22 +13,23 @@ class configulator:
 		config = configparser.ConfigParser()
 		# if it does not exist write some defaults to file
 		if not os.path.isfile('fogofwar.cfg'):
+			print("Creating new Config File")
 			# honey section
 			config.add_section('HONEY')
 			config.set('HONEY','ports',"135,445,22,1433,3389,8080,21,5900,25,53,110,1723,1337,10000,5800,44443,5060")
 
 			#log section
 			config.add_section('LOGS')
-			config.set('LOGS','facility','LOG_LOCAL7')
+			config.set('LOGS','facility','local7')
 
 			#control section
 			config.add_section('CONTROL')
 			config.set('CONTROL','whitelist','127.0.0.1/32')
-			config.set('CONTROL','sensorname','Fog11')
+			config.set('CONTROL','sensorname','MyFog')
 
 			#quarantine section
 			config.add_section('QUARANTINE')
-			config.set('QUARANTINE','quarantine','on')
+			config.set('QUARANTINE','quarantine','off')
 			config.set('QUARANTINE','seconds','3600')
 
 			#quarantine section
@@ -40,7 +41,7 @@ class configulator:
 			config.set('QBGP','port','')
 
 			# write out the file
-			configfile = open('fogofwar.cfg','wb')
+			configfile = open('fogofwar.cfg','w')
 			config.write(configfile)
 			configfile.close()
 	
